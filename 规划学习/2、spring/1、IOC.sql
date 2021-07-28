682bd42a88bbf758255ef2e9028ec84aa9e81550
iamztk <1274200607@qq.com>
2021-07-29 07:12:12 +0800

    提交

--- 规划学习/2、spring/1、IOC.sql
+++ 规划学习/2、spring/1、IOC.sql
@@ -38,3 +38,95 @@
 		--一般会选择在加载配置文件的时候进行创建对象，因为文件加载一般是服务器启动的时候，
 		所以选择件创建对象这种耗时的操作，在启动时完成，后续应用中，就不需要在花费时间创建
 		对象了
+		
+		
+2、IOC操作bean管理
+	 
+	a、什么是bean管理？
+		bean管理指的是两个操作：
+			1、Spring创建对象
+			2、Spring属性注入
+			
+	b、bean管理有两种操作方式：
+		1、基于XML配置文件方式实现
+		2、基于注解方式实现
+		
+	
+	c、基于XML配置文件方式：
+		1、通过get/set实现属性注入
+		2、通过有参构造器实现属性注入
+		
+		方式1、通过get/set注入：
+		实体类：
+			public class Book {
+				public String bname;
+				public String price;
+				public void testDemo(){
+					System.out.println(bname + "===" + price);
+				}
+				public String getBname() {
+					return bname;
+				}
+				public void setBname(String bname) {
+					this.bname = bname;
+				}
+				public String getPrice() {
+					return price;
+				}
+				public void setPrice(String price) {
+					this.price = price;
+				}
+			}
+		XML配置：
+			<bean id="book" class="com.itguigu.spring.Book">  --注意如果bean是这样写，则生成对象是调用的无参构造器
+				<!--属性注入-->
+				--name: 属性名称
+				--value：属性值
+				------这种方式是通过get/set注入
+				<property name="bname" value="java语言面向对象"></property>
+				<property name="price" value="22"></property>
+
+			</bean>
+		
+		方式二：通过有参构造器注入：
+			<!--构造器注入-->
+			--通过配置参数来确定是调用该类的哪个构造方法创建对象。
+			<bean id="orders" class="com.itguigu.spring.Orders">
+				<constructor-arg name="oname" value="爱疯" />
+				<constructor-arg name="price" value="188" />
+			</bean>
+			
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
