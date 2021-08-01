dc13f60a5f1edba1f37c056a7855c6a088272380
iamztk <1274200607@qq.com>
2021-08-02 07:50:11 +0800

    IOC

--- 规划学习/2、spring/1、IOC.sql
+++ 规划学习/2、spring/1、IOC.sql
@@ -240,14 +240,181 @@
 			</property>
 		</bean>
 	
+9、提取list、set、map中存储对象时：
+	例如：
+		list中存放的是Book对象，该如何引用：List<Book>
+			<ref bean= "XXXXX">
+			
+	 <!--引入Book-->
+    <bean id="book" class="com.itguigu.spring.demo4.Book">
+        <property name="bname" value="语文" />
+    </bean>
+
+    <bean id="book2" class="com.itguigu.spring.demo4.Book">
+        <property name="bname" value="数学" />
+    </bean>
+
+    <bean id="stu" class="com.itguigu.spring.demo4.Stu">
+        <property name="list">
+           <list>
+               <ref bean="book" />
+               <ref bean="book2" />
+           </list>
+        </property>
+    </bean>
+	
+10、提取公共的list，复用list
+	--使用<util:list>
+	--需在修改约束如下：
+		原约束：
+		<beans xmlns="http://www.springframework.org/schema/beans"
+		   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		   xmlns:p="http://www.springframework.org/schema/p"
+		   xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd">
+		修改后约束：
+		<beans xmlns="http://www.springframework.org/schema/beans"
+		   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		   xmlns:p="http://www.springframework.org/schema/p"
+		   xmlns:util="http://www.springframework.org/schema/util"  --新增
+		   xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd
+							   http://www.springframework.org/schema/util http://www.springframework.org/schema/util/spring-util.xsd">
+								--上一条为新增
+
+
+	--2、所以定义了多个book	
+    <bean id="book1" class="com.itguigu.spring.demo5.Book">
+        <property name="bname" value="语文" />
+    </bean>
+    <bean id="book2" class="com.itguigu.spring.demo5.Book">
+        <property name="bname" value="数学" />
+    </bean>
+    <bean id="book3" class="com.itguigu.spring.demo5.Book">
+        <property name="bname" value="英语" />
+    </bean>
+	
+	--使用List集合做抽取，是的list集合为公共的
+    <util:list id="booklist">
+        <ref bean="book1" />
+        <ref bean="book2" />
+        <ref bean="book3" />
+    </util:list>
+
+   
+	--1、Stu中包含了List<Book>
+    <bean id="stu" class="com.itguigu.spring.demo5.Stu">
+        <property name="list" ref="booklist" />
+    </bean>
 	
 	
 	
+8、工厂bean（FactoryBean）和普通bean
+	
+	 <bean id="stu" class="com.itguigu.spring.demo5.Stu">
+        <property name="list" ref="booklist" />
+    </bean>
+	
+	如上在xml中的bean定义，定义的事Stu类型的，则在使用时返回的就是Stu类
+	
+	工厂bean书写方式：
+		--需事先Factor有Bean类， 并在getObject中设置返回的对象。
+		public class MyBean implements FactoryBean {
+			@Override
+			public Object getObject() throws Exception {
+			--自定义的需返回的对象
+				Course course = new Course();
+				course.setCname("语文");
+				return course;
+			}
+
+			@Override
+			public Class<?> getObjectType() {
+				return null;
+			}
+
+			@Override
+			public boolean isSingleton() {
+				return false;
+			}
+		}
+		
+		--2、在XML中的定义还是以该实现类来定义：
+			   <bean id="mybean" class="com.itguigu.spring.demo6.factorybean.MyBean">
+			   </bean>
+		
+		--获取对象如下：
+		 ApplicationContext app =
+                    new ClassPathXmlApplicationContext("com\\itguigu\\spring\\demo6\\bean6.xml");
+        Course mybean = app.getBean("mybean", Course.class);
+        mybean.study();
+		
+9、bean的作用域
+	bean有常用的两种：单实例bean 和 多实例bean
+		--配置文件中使用scope进行bean作用域的设置。
+		单实例bean：每次在加载配置文件时，就会创建对应的对象，后续每次使用对象时，都是直接获取该创建的对象，
+	所以，每次获取的对象地址都是一样的。 
+		--注：在配置文件中，默认就是单例bean。
+		配置文件如下：
+			  <bean id="stu" class="com.itguigu.spring.demo4.Stu" scope="singleton">
+			  或者：
+			  <bean id="stu" class="com.itguigu.spring.demo4.Stu">
+		
+		多实例bean，又称：原型bean, 加载配置文件的时候，不会创建对象，而是在getBean()的时候，才会创建对象。
+	所以，每次获取的对象都是不一样的
+		配置文件如下：
+		  <bean id="stu" class="com.itguigu.spring.demo4.Stu" scope="prototype">
+		
+		另外还有两个不常用的作用于：
+			request:存在于一次请求当中
+			session:在一次会话中存在
 	
 	
+10、bean的生命周期
+	1、生命周期
+		从对象的创建到对象销毁的过程
 	
+	2、bean的生命周期（5步）
+		第一步、通过构造器创建bean实例（无参数构造器）
+		第二步、为bean 的属性设置值和对其他bean 的引用（调用set方法）
+		第三步、调用备案的初始化方法（需要配置初始化方法） init-metjiod
+		第四步、bean可以使用了（对象获取到了）
+		第五步、当容器关闭时，调用bean的销毁方法（需要进行配置销毁的方法）
+			destory-method
+			
 	
+	3、bean的生命周期（7步）
+		在第"d"步的前后还有各一步，为调用spring-bean的前后置方法。
+		--调用bean的前置方放弃啊
+		bean可以使用了（对象获取到了）
+		--调用bean 的后置方法。
+		这两步需要配置才能生效，配置如下：
+			编写一个额外的类，该类需事先BeanPostProcessor：
+				public class Book implements BeanPostProcessor {
+					实现接口内的两个方法
+				}
+				--进行xml的配置
+				  <bean id="book" class="com.itguigu.spring.demo4.Book">
+					--这个中间不需要加任何东西，只表明这个book是个前后置处理类
+				  </bean>
+
 	
+11、基于XML实现自动装配：
+		    <bean id="book" class="com.itguigu.spring.demo4.Book" autowire="byName">
+			<bean id="book" class="com.itguigu.spring.demo4.Book" autowire="byType">
+			
+12、外部属性文件：
+		引入属性文件：jdbc.properties
+		需要在xml中配置名称空间：context
+		xmlns:p="http://www.springframework.org/schema/context"
+		
+		<comtext:property-placeholder location="classpath:jdbc.properties"></comtext:property-placeholder>
+		加载配置文件jdbc.properties
+
+		<bean id=dataSource class="com.alibaba.druid.pool.DruidDataSource">
+			<properties name="driverClassName" value="${prop.driverClass}"/>
+			<properties name="url" value="${prop.url}"/>
+			<properties name="username" value="${prop.username}"/>
+			<properties name="password" value="${prop.password}"/>
+		</bean>
 	
 	
 	
