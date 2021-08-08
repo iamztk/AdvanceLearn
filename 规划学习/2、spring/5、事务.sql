7110b658554f502c7549c2e22d86241fd79e48a9
iamztk <1274200607@qq.com>
2021-08-09 07:40:28 +0800

    jdbc+log4j

--- 规划学习/2、spring/5、事务.sql
+++ 规划学习/2、spring/5、事务.sql
@@ -76,19 +76,128 @@
 	在@Transactional（value=""）
 	是可以配置value的值，value的值有如下几种：
 	1、propagation:事务传播行为
-	
+			方法一：
+				@Transaction(propagation = "Propagation.required")
+				public void add(){
+					
+					update();
+				}
+			方法二：
+				public void update(){
+				
+				}
+			事务传播行为可以由传播属性指定： 
+			
+				1、required:
+						如果有事务在运行，当前的方法就在这个事务内运行，
+					否则就启动一个新的事务，并在自己的事务内运行。									
+						问：如果原来有事务，则该方法在这个事务中启用，如果出现
+					异常，那么这个事务是全部回滚吗？
+				2、qeruires_new：
+						不管add()中是否有事务，反正在执行update()时，否会开启
+					一个新事务，并将add()中的事务挂起。
+				3、supports:
+						add()中有事务，则update()在该事务中运行，否则，
+					update()不开启事务。
+				4、Not_supporte...:
+						update()方法，不运行在事务中，如果add()有事务，则挂起。
+				5、mandatory:
+						update()方法必须运行在事务中，没有事务则抛异常。
+				6、Nested：
+						add()有事务，则update()在该事务内运行，否则
+					update()则启动一个事务，并在自己的事务内部运行。
+					
 	2、ioslation：事务隔离级别
+		    @Transactional(propagation= Propagation.REQUIRED, isolation= Isolation.REPEATABLE_READ)
+			四大事务隔离级别：
+				read uncommitted
+				read commited
+				repeatable read
+				serialable
 	
 	3、timeout：超时时间
+		(1)事务需要在一定的时间内进行提交，如果不提交则进行回滚
+		(2)默认值为-1， 即没有超时时间，单位是秒
+		@Transactional(timeout = 5)
 	
 	4、readOnly：是否只读
+		(1)默认值为false，可以进行修改
+	    @Transactional(readOnly = false)
+
 	
 	5、rollbackFor：回滚
+	    @Transactional(rollbackFor = Exception.class)
+		所以异常都回滚。
+		如果是有具体的
+		    @Transactional(rollbackFor = NullPointerException.class)
+		则只有在空指针异常的时候，才会回滚。
 	
 	7、noRollbackFor：不回滚
+		与上相反。
+
 	
-	aa
+8、使用XML完成声明式事务配置
+	 <!--配置事务通知-->
+    <tx:advice id="txadvice">
+        <!--配置事务参数-->
+        <tx:attributes>
+            <tx:method name="insert" propagation="REQUIRED"/>
+            <!--<tx:method name="inser*" progapation="REQUIRED>-->
+            <!--通配符匹配，所有以inser为前缀的都添加该事务传播行为-->
+        </tx:attributes>
+    </tx:advice>
+
+    <!--配置切入点和切面：aop的典型应用就是事务-->
+    <aop:config>
+	--spring.tx.中所有的类中所有的方法都有配置事务，并配置其事务传播行为为required
+        <aop:pointcut id="pt" expression="execution(* com.itguigu.spring.tx.*.*.*(..))"/>
+        <aop:advisor advice-ref="txadvice" pointcut-ref="pt" ></aop:advisor>
+    </aop:config>
 		
+		
+9、完全注解的方式进行编写（不再使用xml中的配置）
+/**
+ * 完全注解方式编写
+ */
+@Configuration
+@ComponentScan(value = "com.itguigu.spring.tx")
+@EnableAspectJAutoProxy
+public class SpringConfig {
+
+    /**
+     * 配置连接池
+     * @Bean：则spring会默认创建该对象，存放于单例池中
+     */
+    @Bean
+    public DruidDataSource getDruidDataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mybatis");
+        dataSource.setUsername("root");
+        dataSource.setPassword("admin");
+        return dataSource;
+    }
+
+    /*
+    *配置JDBC
+    */
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
+
+    /**
+     * 配置事务管理器
+     */
+    @Bean
+    public DataSourceTransactionManager getDataSourceTransactionManager(DataSource dataSource) {
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
+    }
+}
 		
 		
 		
---
+++ 规划学习/2、spring/6、其他功能.sql
@@ -0,0 +1,12 @@
+1、Spring5整合日志
+	1、在整个Spring5框架的代码基于Java8，运行时兼容java9,许多不建议使用的类
+和方法在代码库中已经删除。
+	
+	2、Spring5.0框架自带了通用的日志封装。
+		1、Spring5中移除了Log4ConfigListener, 官方建议使用Log4j2
+		2、Spring5框架整合Log4j2
+		第一步：引入jar包
+			log4j-api, log4j-core, log4j-slf4j, slf4j
+		第二步：创建log4j2.xml配置文件：
+			文件名称就是这个，固定了，不能更改。
+	
